f676b7df3a4cc94c8a1cd2bc617b9c5599b40558
David MTZD <davidmtzd21512@gmail.com>
2025-02-10 17:44:16 -0600

    Agregar el componente Sumar al módulo principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SumarComponent } from "./sumar/sumar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SumarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
