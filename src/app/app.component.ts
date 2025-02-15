94115f11ed19f00577ba5bb3a969c97b1a99276f
David MTZD <davidmtzd21512@gmail.com>
2025-02-15 15:21:40 -0600

    Reemplazar componente Sumar por componente Calculadora en la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SumarComponent } from "./sumar/sumar.component";
 
+import { CalculadoraComponent } from "./calculadora/calculadora.component";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SumarComponent],
+  imports: [RouterOutlet, CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sumar';
+  
 }
