84c03b0ab3d3991b84701142e7c782044531cc1e
David MTZD <davidmtzd21512@gmail.com>
2025-02-16 17:01:41 -0600

    Agregar componente Resultado a la Calculadora y permitir que el resultado sea nulo

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { FormularioComponent } from "./formulario/formulario.component";
+import { ResultadoComponent } from "./resultado/resultado.component";
 
 @Component({
   selector: 'app-calculadora',
-  imports: [FormularioComponent],
+  imports: [FormularioComponent, ResultadoComponent],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
 
-  resultado:number=0;
+  resultado:number| null=null;
   procesarResultado(event : number){
     this.resultado=event;
   }
